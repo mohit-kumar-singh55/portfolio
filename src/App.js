1c5cc22856f35124159e5673da5cf379eb69e46a
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2021-12-22 14:19:38 +0530

    Home is completed

--- src/App.js
+++ src/App.js
@@ -4,15 +4,24 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Transtition from './components/Transition/Transition';
+import BgAnimation from './components/BackgroundAnimation/BackgroundAnimation';
 
 function App() {
   return (
     <div className="gradient__bg">
       <Transtition />
       <div className='late'>
+        {/* BgAnimation */}
+        <BgAnimation />
+
+        {/* Bubble Animations */}
+        <div class="animateme1 css-1tokw9w"></div>
+        <div class="animateme2 css-jfjuh7"></div>
+
         {/* SideBar */}
-          <div id="progressbar"></div>
-          <div id="scrollpath"></div>
+        <div id="progressbar"></div>
+        <div id="scrollpath"></div>
+
         {/* Main Content */}
         <Header />
         <Navbar />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Hero from './Hero/Hero';
-import BgAnimation from './BackgroundAnimation/BackgroundAnimation';
 
 function Home() {
     return (
         <div>
             <Hero />
-            <BgAnimation className="bgAnimation"/>
         </div>
     )
 }
