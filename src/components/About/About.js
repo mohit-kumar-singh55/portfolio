ef2c14e3127261ec5f082594b1e8c184d512340f
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2021-12-24 23:04:35 +0530

    Created About Page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import "./About.css";
 import cpp from "./images/cpp.jpg";
 import js from "./images/js.png";
@@ -7,8 +7,28 @@ import py from "./images/py.png";
 import go from "./images/go.png";
 import swift from "./images/swift.png";
 import ts from "./images/ts.png";
+import $ from "jquery";
+import technology from "./technology.svg";
+import slanguages from "./slanguages.svg";
 
 function About() {
+    useEffect(() => {
+        $('.skill-per').each(function () {
+            var $this = $(this);
+            var per = $this.attr('per');
+            $this.css("width", per + '%');
+            $({ animatedValue: 0 }).animate({ animatedValue: per }, {
+                duration: 1000,
+                step: function () {
+                    $this.attr('per', Math.floor(this.animatedValue) + '%');
+                },
+                complete: function () {
+                    $this.attr('per', Math.floor(this.animatedValue) + '%');
+                }
+            });
+        });
+    }, [])
+
     return (
         <div className="container">
             <div className="About-Me">
@@ -21,14 +41,14 @@ function About() {
                         <p>Welcome to my Arena</p>
                         <p>My name is <strong>Mohit Kumar Singh</strong></p>
                         <p>I am currently in 2nd year of Bsc Computer Science</p>
-                        <br/>
+                        <br />
                         <p>I love to play with DOM</p>
-                        <br/>
+                        <br />
                         <p>I'm quite confident, curious and <em>consistently working to improve and expand my skills</em></p>
-                        <br/>
+                        <br />
                         <p>Looking forward to gain some experience</p>
-                        <br/>
-                        <p><em><strong>~Thanks you for your time</strong></em></p>
+                        <br />
+                        <p><em><strong>~Thank you for your time</strong></em></p>
                     </div>
                 </div>
             </div>
@@ -44,9 +64,76 @@ function About() {
                 </div>
             </div>
             <div className="Technology-">
-
+                <div className='technology_box'>
+                    <span className='dot dot1'></span>
+                    <span className='dot dot2'></span>
+                    <span className='dot dot3'></span>
+                    <h2 className='gradient__text technology_heading'>Technologies</h2>
+                    <div className='technology_box_content'>
+                        <div className='technology_skills'>
+                            <div className="skills">
+                                <div className="skill">
+                                    <div className="skill-name">ReactJS</div>
+                                    <div className="skill-bar">
+                                        <div className="skill-per" per="70"></div>
+                                    </div>
+                                </div>
+                                <div className="skill">
+                                    <div className="skill-name">NodeJS</div>
+                                    <div className="skill-bar">
+                                        <div className="skill-per" per="60"></div>
+                                    </div>
+                                </div>
+                                <div className="skill">
+                                    <div className="skill-name">MangoDb</div>
+                                    <div className="skill-bar">
+                                        <div className="skill-per" per="80"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='technology_svg'>
+                            <img className='technologySVG' src={technology} alt="Technology SVG" />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className="s_languages">
+                <div className='s_languages_box'>
+                    <span className='dot dot1'></span>
+                    <span className='dot dot2'></span>
+                    <span className='dot dot3'></span>
+                    <h2 className='gradient__text s_languages_heading'>Speaking Languages</h2>
+                    <div className='s_languages_box_content'>
+                        <div className='s_languages_svg'>
+                            <img className='s_languagesSVG' src={slanguages} alt="Speaking Languages" />
+                        </div>
+                        <div className='s_languages_skills'>
+                            <div className="skills">
+                                <div className="skill">
+                                    <div className="skill-name">Hindi</div>
+                                    <div className="skill-bar">
+                                        <div className="skill-per" per="100"></div>
+                                    </div>
+                                </div>
+                                <div className="skill">
+                                    <div className="skill-name">English</div>
+                                    <div className="skill-bar">
+                                        <div className="skill-per" per="70"></div>
+                                    </div>
+                                </div>
+                                <div className="skill">
+                                    <div className="skill-name">Japanese</div>
+                                    <div className="skill-bar">
+                                        <div className="skill-per" per="50"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
     )
 }
 
