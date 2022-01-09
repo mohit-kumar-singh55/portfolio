38e39e9cf21c8203d341ab0734304577c9fc7d91
Mohit Kumar Singh <mohitkumarsingh12344@gmail.com>
2022-01-09 22:19:40 +0530

    links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,10 +9,10 @@ function Navbar() {
         <div>
             <nav>
                 <ul>
-                    <li><Link className='a' style={{color:`${location.pathname === '/mks.github.io/' ? "red" : "cyan"}`}} to="/mks.github.io/"><i className="fas fa-house-damage"></i></Link></li>
-                    <li><Link className='a' style={{color:`${location.pathname === '/mks.github.io/about' ? "red" : "cyan"}`}} to="/mks.github.io/about"><i className="fas fa-user-tie"></i></Link></li>
-                    <li><Link className='a' style={{color:`${location.pathname === '/mks.github.io/projects' ? "red" : "cyan"}`}} to="/mks.github.io/projects"><i className="fas fa-laptop-code"></i></Link></li>
-                    <li><Link className='a' style={{color:`${location.pathname === '/mks.github.io/contact' ? "red" : "cyan"}`}} to="/mks.github.io/contact"><i className="far fa-address-card"></i></Link></li>
+                    <li><Link className='a' style={{color:`${location.pathname === '/' ? "red" : "cyan"}`}} to="/"><i className="fas fa-house-damage"></i></Link></li>
+                    <li><Link className='a' style={{color:`${location.pathname === '/about' ? "red" : "cyan"}`}} to="/about"><i className="fas fa-user-tie"></i></Link></li>
+                    <li><Link className='a' style={{color:`${location.pathname === '/projects' ? "red" : "cyan"}`}} to="/projects"><i className="fas fa-laptop-code"></i></Link></li>
+                    <li><Link className='a' style={{color:`${location.pathname === '/contact' ? "red" : "cyan"}`}} to="/contact"><i className="far fa-address-card"></i></Link></li>
                 </ul>
             </nav>
         </div>
